95e438d0178ada7dacc46f72a1a9e8cdf22556f7
anwu1219 <anwu@andrews-mbp-3.wireless.davidson.edu>
2016-06-16 11:17:22 -0400

    test.sj

--- myProgram/test.sh
+++ myProgram/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+y=10
 for i in {1..100}
 do
-y=10
-#for file in $i-*.dimacs
-#do
-#j=$(((i-1)/y))
-#j=$((j+1))
+for file in $i-*.dimacs
+do
+j=$(((i-1)/y))
+j=$((j+1))
+echo $j
 #mv $i-*.dimacs f$j
-#done
+done
 done
