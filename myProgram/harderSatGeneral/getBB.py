8869931aa1f23204dc7525259a1df56f2d91b8ad
anwu <anwu@davidson.edu>
2016-06-28 10:50:12 -0400

    new example structure

--- myProgram/harderSatGeneral/getBB.py
+++ myProgram/harderSatGeneral/getBB.py
@@ -1,70 +1,196 @@
 
 import os
 import sys
-
+import random
+import copy
 
 """
 This file is used to obtain training data.
 """
 def main():
+    content = []
     original_cnf = sys.argv[1]
     with open(original_cnf, 'r') as in_file: # Read a dimacs file
-        in_content = in_file.readlines()
-    
-    SAT = 0
-    with open(original_cnf.split()[0] + "Sol.txt", 'r') as in_file:
-        content = in_file.readlines()
-        if "UNSAT" not in content[0]:
-            SAT = 1
-        else: 
-            solution = map(int, content[0].split())
-            
-
-    temp = in_content[1].split()
-    n_vars = int(temp[2]) # Get the number of variable
-
-    
-    
-    for i in range(n_vars+1)[1:]:
-        new_dimacs_name_pos = original_cnf.split("/")[-1].split(".")[0]+"-" + str(i) + ".dimacs"
-        write_file(i, new_dimacs_name_pos, in_content)
+        for line in in_file:
+            if len(content) == 1:
+                content.append(line.split())
+            elif len(content) >= 2:
+                content.append(map(int, line.split()))
+            else:
+                content.append(line)
+    with open(original_cnf.split(".")[0] + ".txt", 'r') as in_file:
+        contentS = in_file.readlines()
+    if "UNSAT" in contentS[0]:
+        UNSAT_original_formula(original_cnf, content)
+    elif "INDET" in contentS[0]:
+        pass
+    else: 
+        solution = map(int, contentS[1].split())
+        write_UNSAT(original_cnf, copy.deepcopy(content))
+#        write_SAT_file(original_cnf, content, solution[:-1])
 
 
-
-
-def write_file(lit, new_dimacs_name, in_content):
-    deleted_clause = 0
-    new_dimacs = [in_content[0],[]]
-    for line in in_content[2:]:
-        int_lst = map(int, line.split())
-        if lit not in int_lst:
-            if -lit in int_lst:
-                int_lst.remove(-lit)
-            new_dimacs.append(" ".join(map(str, int_lst)))
-        else:
-            deleted_clause += 1
-    info = in_content[1].split()
-    info[2] = str(int(info[2]) - 1)
-    info[3] = str(int(info[3]) - deleted_clause)
-    new_dimacs[1] = " ".join(info) # update the number of clauses
-    with open(new_dimacs_name, 'w') as out_file:
-        out_file.write(new_dimacs[0]) # The first line has change line itself
-        out_file.write(new_dimacs[1] + "\n")
-        for line in new_dimacs[2:]:
-            line = " ".join(update_line(line, abs(lit)))
-            out_file.write(line + "\n")
-    with open(new_dimacs_name.split()[0]+"Sol.txt", 'w') as out_file:
-        
+def UNSAT_original_formula(original_cnf, content):
     return
 
 
+
+
+def write_UNSAT(original_cnf, content):
+    i = 1
+    unsat_lit_file = original_cnf.split('.')[0]+ '-' + str(i) + ".ulit"
+    while(os.path.isfile(unsat_lit_file)):
+        print i
+        with open(unsat_lit_file, 'r') as in_answer:
+            first = in_answer.readline().split()
+            second = in_answer.readline().split()
+            if len(second) !=0 : 
+                lits = map(int, first[: len(second) + 1])[1:]
+            else:
+                i += 1
+                unsat_lit_file = original_cnf.split('.')[0]+ '-' + str(i) + ".ulit"
+                continue
+        new_content = copy.deepcopy(content)
+        write_UNSAT_file(unsat_lit_file.split(".")[0], new_content, lits)
+        i += 1
+        unsat_lit_file = original_cnf.split('.')[0]+ '-' + str(i) + ".ulit"
+
+
+
+def write_UNSAT_file(filename, content, lits):
+    """
+    Takes in a satisfiable formula, and takes in a wrong lits assignment, generates a .dimacs if the resulting formula does not contain empty clauses  
+    """
+    while len(lits) > 0:
+        new_dimacs, lits = update_content(content, lits)
+        content, lits = unit_propagation(new_dimacs, lits)
+       #content, lits = shrink_formula(new_dimacs, lits)
+    if not contains_empty(content):
+        with open("u" + filename + ".dimacs", 'w') as out_file:
+            out_file.write(content[0]) # The first line has change line itself
+            for line in content[1:]:
+                out_file.write(' '.join(map(str,line)) + "\n")
+
+
+    
+
+
+def write_SAT_file(original_cnf, in_content, solution):
+    if len(solution) < 1:
+        return
+    new_dimacs, solution = update_content(in_content, solution)
+    new_dimacs, solution = unit_propagation(new_dimacs, solution)
+    new_dimacs, solution = shrink_formula(new_dimacs, solution)
+
+    if len(original_cnf.split('-')) == 1:
+        new_original_cnf = original_cnf.split('.')[0]+ '-' + "1.dimacs"
+    else:
+        new_original_cnf = original_cnf.split('-')[0]+ '-' + str (int (original_cnf.split('-')[1].split('.')[0]) + 1) + ".dimacs"
+
+    with open(new_original_cnf, 'w') as out_file:
+        out_file.write(new_dimacs[0])
+        for line in new_dimacs[1:]:
+            out_file.write(' '.join(map(str,line)) + "\n")
+    with open(new_original_cnf.split('.')[0]+".txt", 'w') as out_file:
+        out_file.write(" ".join(map(str, solution)) + "\n")
+
+    write_SAT_file(new_original_cnf, new_dimacs, solution)
+
+
+#---------------------------------------- Helper methods ----------------------------------------#
+
+def update_content(content, solution):
+    lit = solution[-1]
+    solution.remove(lit)
+    deleted_clause = 0
+    new_dimacs = [content[0],[]]
+    for line in content[2:]:
+        if lit not in line:
+            if -lit in line:
+                line.remove(-lit)
+            new_dimacs.append(line)
+        else:
+            deleted_clause += 1
+    info = content[1]
+    info[2] = int(info[2]) - 1
+    info[3] = int(info[3]) - deleted_clause
+    new_dimacs[1] = info  # update the number of clauses 
+    for i in range(2, len(new_dimacs)):
+        new_dimacs[i] = update_line(new_dimacs[i], abs(lit))
+    solution = update_sol(solution, abs(lit))
+    return new_dimacs, solution
+
+def update_sol(solution, var):
+    for i in range(len(solution)):
+        if abs(solution[i]) > var:
+            sign = solution[i] / abs(solution[i])
+            solution[i] = sign * (abs(solution[i])-1)
+    return solution
+
 def update_line(line, var):
-    linel = map(int, line.split())
-    for i in range(len(linel)):
-        if abs(linel[i]) > var:
-            sign = linel[i] / abs(linel[i])
-            linel[i] = sign * (abs(linel[i]) - 1)
-    return map(str, linel)
+    """
+    decrease the numbers of variables by 1
+    """
+    for i in range(len(line)):
+        if abs(line[i]) > var:
+            sign = line[i] / abs(line[i])
+            line[i] = sign * (abs(line[i]) - 1)
+    return line
+
+
+def unit_propagation(content, solution):
+    for i in range(2, len(content)):
+        if len(content[i]) == 2:
+            lit = content[i][0]
+            deleted_clause = 0
+            new_dimacs = [content[0],[]]
+            for line in content[2:]:
+                if lit not in line:
+                    if -lit in line:
+                        line.remove(-lit)
+                    new_dimacs.append(line)
+                else:
+                    deleted_clause += 1
+            for j in range(2, len(new_dimacs)):
+                new_dimacs[j] = update_line(new_dimacs[j], abs(lit))
+            if lit in solution:
+                solution.remove(lit)
+            else:
+                print "can't find", lit
+            solution = update_sol(solution, abs(lit))
+            info = content[1]
+            info[2] = int(info[2]) - 1
+            info[3] = int(info[3]) - deleted_clause
+            new_dimacs[1] = info # update the number of clauses
+            return unit_propagation(new_dimacs, solution)
+    return content, solution
+
+
+def shrink_formula(content, solution):
+    for ele in range(len(solution)+1)[1:]:
+        if (not any(-ele in line for line in content[2:])) and (not any(ele in line for line in content[2:])):
+            for i in range(2, len(content)):
+                content[i] = update_line(content[i], abs(ele))
+            try:
+                if ele in solution:
+                    solution.remove(ele)
+                else:
+                    solution.remove(-ele)
+            except ValueError:
+                print ""
+            solution = update_sol(solution, abs(ele))
+            content[1][2] -= 1
+            return shrink_formula(content, solution)
+    return content, solution
+
+
+def contains_empty(content):
+    for ele in content[2:]:
+        if len(ele) == 1:
+            return True
+    return False
+
+
 
 if __name__ == "__main__":
     main()
--- myProgram/python/miniSolv.py
+++ myProgram/python/miniSolv.py
@@ -6,9 +6,18 @@ import subprocess
 # Takes in a dimacs file and generates a solution file and a solver status file
 def main():
         out_name = sys.argv[1].split('.')[0]+'.txt'
-        if not os.path.isfile(out_name):
-                out = open(out_name,'w')
-                subprocess.call(["/home/anwu/minisat/minisat",sys.argv[1], sys.argv[1].split('.')[0]+'Sol.txt'], stdout=out)
+        if not os.path.isfile(out_name) or is_indet(out_name):
+                out = open(sys.argv[1].split('.')[0]+'Log.txt','w')
+                subprocess.call(["/home/anwu/minisat/core/minisat",sys.argv[1], out_name], stdout=out)
                 out.close()
+
+def is_indet(out_name):
+        with open(out_name, "r") as in_file:
+                content = in_file.readlines()
+                return len(content) == 0 or "INDET" in content[0]
+                        
+
+
 if __name__ == "__main__":
     main()
+
