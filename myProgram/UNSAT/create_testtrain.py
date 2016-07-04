1acb01b8bea7276c16a9f6d93a59a9935ca54017
anwu <anwu@davidson.edu>
2016-07-04 11:01:46 -0400

    delete some files

--- myProgram/UNSAT/create_testtrain.py
+++ myProgram/UNSAT/create_testtrain.py
@@ -1,6 +1,11 @@
 import sys
 import random
 
+"""
+Takes in a data file, and randomly split it into a testset and a trainset
+"""
+
+
 trainset = open(sys.argv[2], 'w')
 testset = open(sys.argv[3], 'w')
 with open(sys.argv[1], 'r') as in_file:
