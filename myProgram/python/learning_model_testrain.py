f6500b4ce626463c8392678ec70d70777144d973
anwu <anwu@davidson.edu>
2016-07-15 09:21:08 -0400

    d

--- myProgram/python/learning_model_testrain.py
+++ myProgram/python/learning_model_testrain.py
@@ -55,21 +55,25 @@ Y_test = []
 with open(TRAIN_FILE_NAME, 'r') as in_file:
 	data_set = in_file.readlines()
 	for line in data_set:
-		line =line.split()[2:] # skip the formula identifier, num_var, and num_clause
-		line = map(float, line)
-                #               X.append([line[0]])
-                X.append(line[:12] + line[14:-1])
-                Y.append(line[-1])
+		line =line.split()[1:] # skip the formula identifier, num_var, and num_clause
+                line = map(float, line)
+                if line[0] == 100:
+                        line = line[1:]
+                        #               X.append([line[0]])
+                        X.append(line[:12] + line[14:-1])
+                        Y.append(line[-1])
 
 with open(TEST_FILE_NAME, 'r') as in_file:
         data_set = in_file.readlines()
         for line in data_set:
-                line =line.split()[2:] # skip the formula identifier, num_var, and num_clause
+                line =line.split()[1:] # skip the formula identifier, num_var, and num_clause                      
                 line = map(float, line)
-#                X_test.append([line[0]])
-#                X_test.append(line[:-1])
-                X_test.append(line[:12] + line[14:-1])
-                Y_test.append(line[-1])
+                if line[0] == 100:
+                        line = line[1:]
+                        #                X_test.append([line[0]])
+                        #                X_test.append(line[:-1])
+                        X_test.append(line[:12] + line[14:-1])
+                        Y_test.append(line[-1])
 
 #scaler = preprocessing.StandardScaler().fit(X)
 #X = scaler.transform(X)
@@ -84,7 +88,7 @@ clf1 = RandomForestClassifier(n_estimators = 100,  n_jobs = -1)
 
 #clf1 = DecisionTreeClassifier()
 #clf1 = KNeighborsClassifier(n_neighbors = 11, n_jobs = -1, weights = 'distance')
-#clf1 = SGDClassifier()
+clf1 = LogisticRegressionCV(Cs=a)
 
 #clf1 = AdaBoostClassifier(n_estimators = 1000)
 # print "Learning..."
@@ -105,6 +109,7 @@ for i in range(len(probs)):
         if probs[i] >= 0.9 or probs[i] <= 0.1:
                 X_test_hp.append(X_test[i])
                 Y_test_hp.append(Y_test[i])
+print len(X_test_hp)
 print "High prob test score:", clf1.score(X_test_hp, Y_test_hp)
 # #predict2 = clf2.score(X_test, Y_test)
 # 	print predict1
