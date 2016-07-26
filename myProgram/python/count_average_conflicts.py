a9c037bf4d830d5f105f3c4ca1190e1c42817d19
anwu <anwu@davidson.edu>
2016-07-25 22:34:12 -0400

    happy

--- myProgram/python/count_average_conflicts.py
+++ myProgram/python/count_average_conflicts.py
@@ -4,12 +4,6 @@ import numpy as np
 lst = []
 with open(sys.argv[1], 'r') as in_file:
     for line in in_file:
-<<<<<<< HEAD
         lst.append(float(line.split()[1]) - 1)
 print "mean number of restart", np.mean(lst)
 print "median number of restart", np.median(lst)
-=======
-        lst.append(float(line.split()[2]))
-print "mean", np.mean(lst)
-print "median", np.median(lst)
->>>>>>> 4c806fe2d4eea75f88b3ccff6200a49df051cb0b
